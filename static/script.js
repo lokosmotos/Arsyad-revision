49aaef08e40baa1488112f577e507588316cb92a
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-18 13:21:39 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,6 @@
-
+function checkAnswer() {
+  const answer = document.getElementById("answer").value;
+  if (answer.includes("saya suka membaca buku")) {
+    document.getElementById("feedback").style.display = "block";
+  }
+}
