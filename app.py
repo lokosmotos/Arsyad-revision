54e3336b67d37c9e901698a953c2a60110776d29
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-18 13:20:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/imlak")
+def imlak():
+    return render_template("imlak.html")
+
+# Add routes for other topics similarly
+
+if __name__ == "__main__":
+    app.run(debug=True)
